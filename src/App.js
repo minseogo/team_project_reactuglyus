44df8e9eb8626835bbc06f457941cbbed10bc9c3
tree4852 <tree4852@naver.com>
2024-08-05 11:54:58 +0900

    footer 추가 및 이미지 경로 수정

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './component/yys/Header_yys';
-import Footer from './component/yjy/Footer';
+import Footer from './component/yys/Footer';
 import Quickmenu from './component/pyj/quick/quickmenu';
  import Maincontent from './component/Maincontent';
 
