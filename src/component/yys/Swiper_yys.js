6adae39d5c19f38c36821f2341cfe210d47ae75f
tree4852 <tree4852@naver.com>
2024-08-06 16:49:15 +0900

    메인배너 이미지 320사이즈 추가

--- src/component/yys/Swiper_yys.js
+++ src/component/yys/Swiper_yys.js
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 import '../../css/yys.css';
@@ -10,57 +10,63 @@ import { Link } from 'react-router-dom';
 
 const Swiper_yys = (props) => {
   const { slides } = swiperdb;
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1400);
+
+  const handleResize = () => {
+    setIsLargeScreen(window.innerWidth > 1400);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
-        <div id="yswiper" className="swiper-container section_blank">
-          <div className="main_caver">
-              <div className="swiper ov mySwiper">
-                  
-                      <Swiper
-                          modules={[Pagination, Autoplay]}
-                          spaceBetween={16}
-                          slidesPerView={1}
-                          centeredSlides={true}
-                          loop={true}
-                          autoplay={{
-                          delay: 6500000,
-                          disableOnInteraction: false,
-                          }}
-
-                          breakpoints={{
-                              768: {
-                                slidesPerView: 1.35,
-                                spaceBetween: 0,
-                              }
-                            }}
-                          
-                          pagination={{
-                          el: '.swiper-pagination',
-                          clickable: true,
-                          
-                      }}
-                      >
-                      {slides.map((v, i) => (
-                          <SwiperSlide key={i}>
-                              <div className="bs mx-md-3" style={{ "background":`url(${v.b_img}) center/ cover`}}>
-                                  <Link to={v.href} className='w-100 h-100'>
-                                    
-                                      
-                                  </Link> 
-                              </div>
-                          </SwiperSlide>
-                      ))}
-                      </Swiper>
-                      
-                  
-              </div>
-              <div className='swiper-pagination'></div>
+      <div id="yswiper" className="swiper-container section_blank">
+        <div className="main_caver">
+          <div className="swiper ov mySwiper">
+            <Swiper
+              modules={[Pagination, Autoplay]}
+              spaceBetween={16}
+              slidesPerView={1}
+              centeredSlides={true}
+              loop={true}
+              autoplay={{
+                delay: 6500,
+                disableOnInteraction: false,
+              }}
+              breakpoints={{
+                768: {
+                  slidesPerView: 1.35,
+                  spaceBetween: 0,
+                }
+              }}
+              pagination={{
+                el: '.swiper-pagination',
+                clickable: true,
+              }}
+            >
+              {slides.map((v, i) => (
+                <SwiperSlide key={i}>
+                  <div 
+                    className="bs mx-md-3" 
+                    style={{ 
+                      background: `url(${isLargeScreen ? v.b_img : v.img}) center/ cover`
+                    }}
+                  >
+                    <Link to={v.href} className='w-100 h-100'></Link>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
+          <div className='swiper-pagination'></div>
+        </div>
       </div>
-    
     </>
-    
   );
 };
 
